584267562dd9afa92d04a15e61acbf88f91bc26b
Dariusz Malinowski <Dariusz.Malinowski.external@ecb.europa.eu>
2022-02-27 21:52:43 +0100

    go

--- examples/main.tf
+++ examples/main.tf
@@ -13,8 +13,6 @@ provider "downtimes" {
   address = "https://epmwd211.t-mgmt.tadnet.net/topaz/bsmservices/customers/1"
   path    = "/downtimes"
   alias   = "dev"
-  username = "obm_api_user"
-  password = "Qwer1234"
 }
 
 
@@ -25,26 +23,14 @@ resource "downtime" "downtime_01" {
   action       = "STOP_MONITORING"
   approver     = "Dariusz Malinowski"
   category     = "OS_CONFIGURATION"
-  selected_cis = "4830ca40d21593b7bf85c3d070b8b8c2"
+  selected_cis = ["4830ca40d21593b7bf85c3d070b8b8c2","45a87733d08497b5963608756d47eae1","471b3e8f086f6aeb90dae487c512aacc"]
   schedule {
     type       = "ONCE"
-    start_date = "2022-02-26T14:40:00+01:00"
+    start_date = "2022-02-27T14:40:00+01:00"
     end_date   = "2022-02-28T14:40:00+01:00"
     timezone   = "Europe/Berlin"
   }
 
 }
 
-/*
-resource "downtime" "downtime_02" {
-  provider     = downtimes.dev
-  name         = "OBM Dowtime second"
-  description  = "Created from terraform. change 1."
-  action       = "STOP_MONITORING"
-  approver     = "Dariusz Malinowski"
-  category     = "SECURITY"
-  selected_cis = "4830ca40d21593b7bf85c3d070b8b8c2"
-  start_date    = "2022-03-01T14:40:00+01:00"
-  end_date      = "2022-03-03T14:40:00+01:00"
-}
-*/
+
